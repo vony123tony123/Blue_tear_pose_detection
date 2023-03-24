11948f4a7eec60ee0341d7d645fd5ff1fe5f3780
JIAN-MING TU <43849007+vony123tony123@users.noreply.github.com>
2023-03-24 23:45:23 +0800

    update socket timeout and test connect

--- socket_client.py
+++ socket_client.py
@@ -17,6 +17,9 @@ pose_dict = ["noPose", "SunRight", "SunLeft", "CloseLight", "WindRight", "WindLe
 HOST = '127.0.0.1'
 PORT = 11000
 
+is_connect = False
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 def get_frame():
   global global_frame
   cap = cv2.VideoCapture(0)
@@ -34,56 +37,79 @@ def get_frame():
       break
   cap.release()
 
+def test_connect():
+  global is_connect
+  print("Testing Connect...")
+  while True:
+    try:
+        is_connect = False
+        s.connect((HOST, PORT))
+        s.settimeout(1)
+        is_connect = True
+        print("Testing Connect Success")
+        break
+    except Exception as e:
+      print("Testing Connect Failed")
+      time.sleep(2)
+
 def detect_frame():
-  global global_frame
-  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
     # For webcam input:
     with mp_holistic.Holistic(
       static_image_mode=False,
       min_detection_confidence=0.5,
       min_tracking_confidence=0.5) as holistic:
       while True:
-        lock.acquire()
-        image = global_frame.copy()
-        lock.release()
+        try:
+          lock.acquire()
+          image = global_frame.copy()
+          lock.release()
 
-        # To improve performance, optionally mark the image as not writeable to
-        # pass by reference.
-        image.flags.writeable = False
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        results = holistic.process(image)
+          # To improve performance, optionally mark the image as not writeable to
+          # pass by reference.
+          image.flags.writeable = False
+          image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+          results = holistic.process(image)
 
-        # Draw landmark annotation on the image.
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+          # Draw landmark annotation on the image.
+          image.flags.writeable = True
+          image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        pose_idx = classify_pose(results, image)
+          pose_idx = classify_pose(results, image)
 
-        if pose_idx is None:
-            pose_idx = 0
+          if pose_idx is None:
+              pose_idx = 0
 
-        s.sendall(bytes(pose_dict[pose_idx], encoding='utf-8'))
-
-        image = draw_landmarks(image, results, mp_holistic, mp_drawing, mp_drawing_styles)
-        image = cv2.flip(image, 1)
-        if pose_idx != None:
-          cv2.putText(image, pose_dict[pose_idx], (10,40),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-        else:
-          cv2.putText(image, pose_dict[0], (10,40),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-        cv2.imshow('MediaPipe Holistic', image)
-
-        data = s.recv(1024)
-        print('Received', repr(data))
-       
-        if cv2.waitKey(1) == ord('q'):
-            break    # 按下 q 鍵停止
+          if is_connect :
+            s.sendall(bytes(pose_dict[pose_idx], encoding='utf-8'), )
+            data = s.recv(1024)
+            print('Received', repr(data))
+          
+          image = draw_landmarks(image, results, mp_holistic, mp_drawing, mp_drawing_styles)
+          image = cv2.flip(image, 1)
+          if pose_idx != None:
+            cv2.putText(image, pose_dict[pose_idx], (10,40),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+          else:
+            cv2.putText(image, pose_dict[0], (10,40),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+          cv2.imshow('MediaPipe Holistic', image)
+         
+          if cv2.waitKey(1) == ord('q'):
+              break    # 按下 q 鍵停止
+        except Exception as e:
+          print(e)
+          s.close()
+          b = threading.Thread(target=test_connect)
+          b.daemon = True
+          b.start()
 
 
 
 a = threading.Thread(target=get_frame)
 a.daemon=True
 a.start()
+time.sleep(2)
+b = threading.Thread(target=test_connect)
+b.daemon = True
+b.start()
 time.sleep(2)
 detect_frame()
 
